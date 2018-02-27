ed07d2e2d5a43095fae14f07fabd814621826396
张宁乐 <1160030724@qq.com>
2018-02-27 12:07:25 +0800

    4

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# 大连罗森手机端
-http://zhangningle.gitee.io/m.luosen/
+# m.luosen
--- js/script.js
+++ js/script.js
@@ -15,10 +15,10 @@ for (var i=0;i<li.length;i++){
     li[i].index=i;
     li[i].onclick=function (ev) {
         for(var i=0;i<box1.length;i++){//先清空所有的样式
-            li[i].lastElementChild.classList.remove("nav_active");  //循环清空li样式
+            li[i].classList.remove("nav_active");  //循环清空li样式
             box1[i].style.display='none';  //循环隐藏所有div
         }
-        this.lastElementChild.classList.add("nav_active");  //当前点击的元素样式变成active
+        this.classList.add("nav_active");  //当前点击的元素样式变成active
         box1[this.index].style.display='block';    //this.index 获取当前li对应的索引
     }
 }
